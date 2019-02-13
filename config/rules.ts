11aa572d718537209ce3d66528178fef0d77a8f7
Lucas Sifoni <lucas@lucassifoni.info>
2019-02-13 10:53:01 +0100

    Adds two first rules as example

--- config/rules.ts
+++ config/rules.ts
@@ -1,5 +1,16 @@
 import { RuleInterface } from '~/lib/RuleInterface'
 
-const rules: RuleInterface[] = []
+const rules: RuleInterface[] = [
+{
+    description: 'Removes spaces before simple punctuations',
+    find: /(\w)\s+([,\.])/gi,
+    replace: "$1$2",
+},
+{
+    description: 'Ensures a space after a simple punctuation',
+    find: /(\w[,\.])\s*(\w)/gi,
+    replace: "$1 $2",
+},
+]
 
 export default rules
