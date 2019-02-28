e14fc3176411e6e9704242c8adae7ef75cfb841d
Lucas Sifoni <lucas@lucassifoni.info>
2019-02-28 15:36:39 +0100

    TSLint fixes

--- config/rules.ts
+++ config/rules.ts
@@ -1,22 +1,22 @@
-import { RuleInterface } from "~/lib/RuleInterface";
+import { RuleInterface } from '~/lib/RuleInterface'
 
-import { LIST } from "~/config/characters";
+import { LIST } from '~/config/characters'
 
 const ordinalNumbersMap = {
-    ere: "re",
-    eres: "re",
-    ère: "re",
-    ères: "res",
-    me: "ème",
-    mes: "èmes",
-    eme: "ème",
-    emes: "èmes"
-};
+    ere: 're',
+    eres: 're',
+    ère: 're',
+    ères: 'res',
+    me: 'ème',
+    mes: 'èmes',
+    eme: 'ème',
+    emes: 'èmes'
+}
 
 export const frenchRules: RuleInterface[] = [
     {
         id: 1,
-        description: "Replaces three dots with an ellipsis",
+        description: 'Replaces three dots with an ellipsis',
         contexts: {
             brut: {
                 find: /\.{3}/gi,
@@ -26,28 +26,28 @@ export const frenchRules: RuleInterface[] = [
     },
     {
         id: 2,
-        description: "Replaces quotes with french quotes",
+        description: 'Replaces quotes with french quotes',
         contexts: {
             brut: {
                 replace(str: string): string {
-                    let open = false;
-                    let output = "";
+                    let open = false
+                    let output = ''
                     for (const char of str) {
                         if (char === '"') {
-                            output += open ? LIST.RQUOTE : LIST.LQUOTE;
-                            open = !open;
-                            continue;
+                            output += open ? LIST.RQUOTE : LIST.LQUOTE
+                            open = !open
+                            continue
                         }
-                        output += char;
+                        output += char
                     }
-                    return output;
+                    return output
                 }
             }
         }
     },
     {
         id: 3,
-        description: "Ensures non-breaking space after opening quote",
+        description: 'Ensures non-breaking space after opening quote',
         contexts: {
             brut: {
                 find: new RegExp(`${LIST.LQUOTE}\s*`),
@@ -57,7 +57,7 @@ export const frenchRules: RuleInterface[] = [
     },
     {
         id: 4,
-        description: "Ensures non-breaking space after closing quote",
+        description: 'Ensures non-breaking space after closing quote',
         contexts: {
             brut: {
                 find: new RegExp(`\s*${LIST.RQUOTE}`),
@@ -67,28 +67,28 @@ export const frenchRules: RuleInterface[] = [
     },
     {
         id: 5,
-        description: "Removes spaces before simple punctuations",
+        description: 'Removes spaces before simple punctuations',
         contexts: {
             brut: {
                 find: /(\w)\s+([,\.])/gi,
-                replace: "$1$2"
+                replace: '$1$2'
             }
         }
     },
     {
         id: 6,
-        description: "Ensures a space after a simple or double punctuation",
+        description: 'Ensures a space after a simple or double punctuation',
         contexts: {
             brut: {
                 find: /(\w[,\.:,?!])\s*(\w)/gi,
-                replace: "$1 $2"
+                replace: '$1 $2'
             }
         }
     },
     {
         id: 7,
         description:
-            "Ensures a single non-breaking space before a double punctuation",
+            'Ensures a single non-breaking space before a double punctuation',
         contexts: {
             brut: {
                 find: /\s*([!?:;])/gi,
@@ -98,7 +98,7 @@ export const frenchRules: RuleInterface[] = [
     },
     {
         id: 8,
-        description: "Ensures a single space after a colon or semicolon",
+        description: 'Ensures a single space after a colon or semicolon',
         contexts: {
             brut: {
                 find: /([:;])\s*/gi,
@@ -108,35 +108,35 @@ export const frenchRules: RuleInterface[] = [
     },
     {
         id: 9,
-        description: "Normalizes ordinal numbers",
+        description: 'Normalizes ordinal numbers',
         contexts: {
             brut: {
                 replace(str: string): string {
-                    const searches: string[] = Object.keys(ordinalNumbersMap);
+                    const searches: string[] = Object.keys(ordinalNumbersMap)
                     const re = new RegExp(
-                        `(\\d+)(${searches.join("|")})`,
-                        "gi"
-                    );
+                        `(\\d+)(${searches.join('|')})`,
+                        'gi'
+                    )
                     return str.replace(
                         re,
                         (_match: string, nums: string, capture: string) =>
                             nums + ordinalNumbersMap[capture]
-                    );
+                    )
                 }
             }
         }
     },
     {
         id: 10,
-        description: "Exposes ordinal numbers",
+        description: 'Exposes ordinal numbers',
         contexts: {
             html: {
                 find: new RegExp(
-                    `(\\d+)(${Object.values(ordinalNumbersMap).join("|")})`,
-                    "gi"
+                    `(\\d+)(${Object.values(ordinalNumbersMap).join('|')})`,
+                    'gi'
                 ),
                 replace: `$1<sup>$2</sup>`
             }
         }
     }
-];
+]
