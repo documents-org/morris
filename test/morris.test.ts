98de6bb580c5b90a5368816b1826f91458cd1004
Lucas Sifoni <lucas@lucassifoni.info>
2019-03-07 12:26:14 +0100

    Adds new tests

--- test/morris.test.ts
+++ test/morris.test.ts
@@ -3,58 +3,174 @@ import { Morris } from "~/lib/index";
 
 const mo = new Morris(frenchRules);
 
-describe("Replaces three dots with an ellipsis", () => {});
+describe("Replaces three dots with an ellipsis", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(`bonjour... il fait beau`, `brut`, 1);
+        expect(formatted).toEqual(`bonjour\u2026 il fait beau`);
+    });
+});
 describe("Replaces quotes with french quotes", () => {
     it("in the context of brut", () => {
         const formatted = mo.apply(`"régler"`, `brut`, 2);
         expect(formatted).toEqual(`«régler»`);
     });
 });
+describe("Replaces smart quotes with french quotes", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(`“salut”`, `brut`, 3);
+        expect(formatted).toEqual(`«salut»`);
+    });
+});
 describe("Ensures non-breaking space after opening quote", () => {
     it("in the context of brut", () => {
-        const formatted = mo.apply(`«régler»`, `brut`, 3);
+        const formatted = mo.apply(`«régler»`, `brut`, 4);
         expect(formatted).toEqual(`«\u00A0régler»`);
     });
 });
 describe("Ensures non-breaking space after closing quote", () => {
     it("in the context of brut", () => {
-        const formatted = mo.apply(`«régler»`, `brut`, 4);
+        const formatted = mo.apply(`«régler»`, `brut`, 5);
         expect(formatted).toEqual(`«régler\u00A0»`);
+    });
+});
+describe("Search multiple spaces in a row to reduce them to a single", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(`bonjour     , il   fait beau`, `brut`, 6);
+        expect(formatted).toEqual(`bonjour , il fait beau`);
     });
 });
 describe("Removes spaces before simple punctuations", () => {
     it("in the context of brut", () => {
-        const formatted = mo.apply(`bonjour     , il fait beau`, `brut`, 5);
-        expect(formatted).toEqual(`bonjour, il fait beau`);
+        const formatted = mo.apply(
+            `bonjour , il fait beau
+`,
+            `brut`,
+            7
+        );
+        expect(formatted).toEqual(`bonjour, il fait beau
+`);
     });
 });
 describe("Ensures a space after a simple or double punctuation", () => {
     it("in the context of brut", () => {
-        const formatted = mo.apply(`bonjour,il fait beau`, `brut`, 6);
-        expect(formatted).toEqual(`bonjour, il fait beau`);
+        const formatted = mo.apply(
+            `bonjour,il fait beau
+`,
+            `brut`,
+            8
+        );
+        expect(formatted).toEqual(`bonjour, il fait beau
+
+`);
     });
 });
 describe("Ensures a single non-breaking space before a double punctuation", () => {
     it("in the context of brut", () => {
-        const formatted = mo.apply(`il est vrai:nous sommes verts`, `brut`, 7);
-        expect(formatted).toEqual(`il est vrai\u00A0:nous sommes verts`);
-    });
-});
-describe("Ensures a single space after a colon or semicolon", () => {
-    it("in the context of brut", () => {
-        const formatted = mo.apply(`il est vrai:nous sommes verts`, `brut`, 8);
-        expect(formatted).toEqual(`il est vrai: nous sommes verts`);
+        const formatted = mo.apply(
+            `il est vrai:nous sommes verts
+`,
+            `brut`,
+            9
+        );
+        expect(formatted).toEqual(`il est vrai\u202F:nous sommes verts
+`);
     });
 });
 describe("Normalizes ordinal numbers", () => {
     it("in the context of brut", () => {
-        const formatted = mo.apply(`la 1ere`, `brut`, 9);
+        const formatted = mo.apply(`la 1ere`, `brut`, 10);
         expect(formatted).toEqual(`la 1re`);
+    });
+});
+describe("Normalizes ordinal numbers plural", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(`les 2emes`, `brut`, 11);
+        expect(formatted).toEqual(`les 2es`);
+    });
+});
+describe("Normalizes ordinal numbers", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(`la 2eme`, `brut`, 12);
+        expect(formatted).toEqual(`la 2e`);
     });
 });
 describe("Ordinal numbers are exposed", () => {
     it("in the context of html", () => {
-        const formatted = mo.apply(`les 2èmes`, `html`, 10);
+        const formatted = mo.apply(`les 2èmes`, `html`, 13);
         expect(formatted).toEqual(`les 2<sup>èmes</sup>`);
+    });
+});
+describe("Modify the suites of letters Mr and Mme by exposed letters after the M", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(
+            `Mme de Bovary
+Mr le Visconsul`,
+            `brut`,
+            14
+        );
+        expect(formatted).toEqual(`Mme de Bovary
+Mr le Visconsul`);
+    });
+    it("in the context of html", () => {
+        const formatted = mo.apply(
+            `Mme de Bovary
+Mr le Visconsul`,
+            `html`,
+            14
+        );
+        expect(formatted).toEqual(`M<sup>me</sup> de Bovary
+M<sup>r</sup> le Visconsul`);
+    });
+});
+describe("Change the writing of the centuries", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(
+            `XVIIIeme siècle
+
+`,
+            `brut`,
+            15
+        );
+        expect(formatted).toEqual(`XVIIIe siècle`);
+    });
+    it("in the context of html", () => {
+        const formatted = mo.apply(`XVIIIe siècle`, `html`, 15);
+        expect(formatted).toEqual(`XVIII<sup>e</sup> siècle`);
+    });
+});
+describe("A word of less than two letters at the end of the line is attached to the following word", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(
+            `le souvenir du
+beau temps`,
+            `brut`,
+            16
+        );
+        expect(formatted).toEqual(`le souvenir 
+du\u00A0beau temps`);
+    });
+});
+describe("A word of less than two capital letters at the end of the line is attached to the previous word", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(
+            `le CROA, les 
+MA et les institutions`,
+            `brut`,
+            17
+        );
+        expect(formatted).toEqual(`le CROA, les\u00A0MA 
+et les institutions`);
+    });
+});
+describe("A single word at the end of the paragraph is attached to the previous word", () => {
+    it("in the context of brut", () => {
+        const formatted = mo.apply(
+            `le souvenir du beau 
+temps`,
+            `brut`,
+            18
+        );
+        expect(formatted).toEqual(`le souvenir du 
+beau\u00A0temps`);
     });
 });
